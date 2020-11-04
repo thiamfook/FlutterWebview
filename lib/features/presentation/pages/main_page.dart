90da9ce96bdb0970df0714ff6fd32a7b6fd767b4
Jason Thai <thiamfook@gmail.com>
2020-11-04 09:10:25 +0800

    iOS support

--- lib/features/presentation/pages/main_page.dart
+++ lib/features/presentation/pages/main_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key key}) : super(key: key);
+  const MainPage({Key key = const Key("any_key")}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
